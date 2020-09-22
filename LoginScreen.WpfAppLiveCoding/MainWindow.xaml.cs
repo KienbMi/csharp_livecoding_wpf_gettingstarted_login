2a9efdd8db04aa0e8f1951e6f6a8768a67477327
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-09-22 19:52:27 +0200

    Implement login window with success window

---
+++ LoginScreen.WpfAppLiveCoding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginScreen.WpfAppLiveCoding
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      btnLogin.Click += BtnLogin_Click;
+    }
+
+    private void BtnLogin_Click(object sender, RoutedEventArgs e)
+    {
+      valUsername.Visibility = txtUsername.Text == "jfuerlinger" ? Visibility.Hidden : Visibility.Visible;
+
+      if(txtUsername.Text == "jfuerlinger")
+      {
+        SuccessWindow wndSuccess = new SuccessWindow();
+        wndSuccess.Show();
+        this.Close();
+      }
+    }
+  }
+}
---
+++ LoginScreen.WpfAppLiveCoding/SuccessWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginScreen.WpfAppLiveCoding
+{
+  /// <summary>
+  /// Interaction logic for SuccessWindow.xaml
+  /// </summary>
+  public partial class SuccessWindow : Window
+  {
+    public SuccessWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
